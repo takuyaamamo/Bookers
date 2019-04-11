63d389fa83d5388ff263f6520f8178198e2406eb
Ama <Ama@amacbook.local>
2019-04-11 18:43:44 +0900

    [fix]Made the model and controller names correspond. Furthermore,Deleted unnecessary postmodels.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # HTTPメソッド 'URL' => 'コントローラ#アクション'
   # トップページをルートにするroot to: 'コントローラ#アクション'
   root to: 'toppage#top'
+
+  post 'books' => 'books#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_081232) do
+ActiveRecord::Schema.define(version: 2019_04_11_093034) do
 
-  create_table "posts", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
