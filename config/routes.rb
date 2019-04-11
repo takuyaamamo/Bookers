cd5c178decfce25dfecc460b8eb1cba0af230da9
Ama <Ama@amacbook.local>
2019-04-11 17:11:27 +0900

    [update]Set the top page as route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # HTTPメソッド 'URL' => 'コントローラ#アクション'
-  get 'top' => 'impression#top'
+  # トップページをルートにするroot to: 'コントローラ#アクション'
+  root to: 'impression#top'
 end
